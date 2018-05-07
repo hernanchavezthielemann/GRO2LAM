d7a6066d5eb56452152a624889d118c9b64d4f64
Hernan Chavez Thielemann <herchavezt@gmail.com>
2018-05-07 18:35:28 +0200

    Update grotolam.py

--- lib/grotolam.py
+++ lib/grotolam.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python
 #    -------------    runing simulations   ----------------
-__version__ = 'GROTOLAM version 1.0 (13 Apr 2018)'#.split()[2]
+__version__ = 'GROTOLAM version 2.0 (07 May 2018)'#.split()[2]
 
+__old_ver__ = ['version 1.0 (13 Apr 2018)']
+
+__url__ = 'https://github.com/hernanchavezthielemann/GRO2LAM'
 #from os import getcwd, walk, chdir, makedirs, system
 #from os.path import join
 #from sys import exit, argv
@@ -31,10 +34,9 @@ class Grotolam(object):
         write_file('content.g2l', output)
 
 def grotolam_launcher():
-    
+    ''' at tis point this is just the name, quite deceptive ;) 
+        maybe <run> can call directly the GUI'''
     print '\n'+' '*20+__version__+'\n'
-    
-    
     
     launch_gui()
     #conv_data, ckbuttons, solva_tags,_quit_flag_ = launch_gui()
