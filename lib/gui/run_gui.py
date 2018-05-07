7778348480f9d561c596f12c773867fc62209f51
Hernan Chavez Thielemann <herchavezt@gmail.com>
2018-05-07 18:24:22 +0200

    Create run_gui.py

---
+++ lib/gui/run_gui.py
@@ -0,0 +1,100 @@
+#!/usr/bin/python
+#    By Hernan Chavez Thielemann
+__author__ = 'Hernan Chavez Thielemann <hchavezthiele at gmail dot com>'
+# checked ok 04/04/2018 
+
+#------------------------------------------------------
+#///    Packages and globals definitions are here   ///
+#------------------------------------------------------
+
+from Tkinter import Frame, Button, Label
+
+from tk_lib import create_entry, get_entriesvalue, bottom_hline_deco
+from lib.misc.data import check_vars
+from lib.misc.file import check_file, run_command
+
+#------------------------------------------------------
+'''///////////////        Class       /////////////'''
+#------------------------------------------------------
+class Run_GUI(Frame):
+    ''' Script creation graphical user interface.
+        Since script gui  was crowding the main
+        in order to neat
+        this is a better place for them
+    '''
+    def __init__(self, master=None, **options):
+        self.master  = master
+        Frame.__init__(self, master)
+        
+
+    def createWidgets(self):
+        'create the script gui'
+        #    first header row
+        row = Frame(self)
+        TEXT1= "\nIn this section you can run scripts "
+        Label(row, text=TEXT1, anchor='w').pack(side='top', padx=2, pady=10)
+        row.pack( side='top', fill='x', padx=5)
+        bottom_hline_deco(self)
+        
+        
+        '''  RUN  section '''
+        entr_maxlen = int(8*2.5)
+        self.r_entry_c = []
+        row2fill = Frame(self)
+        row2fill_l = Frame( row2fill)
+        self.r_entry_c.append( self.master.createfileentry(row2fill_l,
+                                                   'Select the script to run',
+                                                   './in.gro2lam')
+                             )
+        _entries_ = ['Machine', 'Cores']
+        _defvals_=[['lammps-daily',
+                    [ 'lammps', 'lmp_mpi', 'lmp_ubuntu' ,'lammps-daily']],
+                    [ 1, range(4*4*4+1)[1:]] # should be enough
+                  ]
+        for e in range(len(_entries_)):
+            self.r_entry_c.append(create_entry(row2fill_l,
+                                               _entries_[e],
+                                               _defvals_[e],
+                                               entr_maxlen))
+            
+        row2fill_l.pack(side="left")
+        row2fill_r = Frame(row2fill)
+        
+        row2fill_r.pack(side="right")
+        row2fill.pack(side="top", padx=1, pady=5)
+        
+        bottom_hline_deco(self, bottom_hline_deco(self))
+        
+        self.build_finalbuttons()
+        
+    
+    def build_finalbuttons(self):
+        '''    Final Buttons    '''
+        row = Frame(self)#.body)
+        #    Final Button
+        Frame(row).pack(side='top', pady=4)# ------------------------ OJO
+        row.pack(fill='x')
+        
+        row= Frame(self)#.body)
+        self.b1 = Button( row, text='Run',
+                    command=( lambda re= self.r_entry_c :
+                             run_script(*get_entriesvalue(re)))
+                   )
+        self.b1.pack(side='right', padx=15, pady= 5)
+        
+        b2 = Button(row, text='Quit', command= self.quit )
+        b2.pack(side='right', padx=5, pady= 4)
+        row.pack( side='right', fill='both', padx=5)
+        
+
+def run_script( _file_, machine='lammps', _cores_=1):
+    
+    core_flag = min(check_vars([_cores_],['int'],'Run aborted!'))
+    if check_file(_file_) and core_flag:
+        print _file_
+        command = '{} -echo both -in {}'.format( machine, _file_)
+        if _cores_>1:
+            command = 'mpirun -np {} '.format( _cores_) + command
+            
+        run_command(command)
+        
