9612384bf5349588f1ecc2b6691d90c3586e54ec
Hernan Chavez Thielemann <herchavezt@gmail.com>
2018-05-09 17:49:45 +0200

    Create version.py

---
+++ lib/misc/version.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+# pitiful file, but neats
+
+__curr_ver__ = '1.1 07 May 2018'.split()
+
+__old_vers__ = ['1.0 13 Apr 2018']
+
+__url__ = 'https://github.com/hernanchavezthielemann/GRO2LAM'
+
+__version__ = 'GROTOLAM version {} ({} {} {})'.format(*__curr_ver__)
