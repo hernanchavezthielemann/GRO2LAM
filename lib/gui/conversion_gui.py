2475d98be1bfaf73ec79981a07700987346c5e5b
Hernan Chavez Thielemann <herchavezt@gmail.com>
2019-01-27 18:46:45 +0100

    Update conversion_gui.py

--- lib/gui/conversion_gui.py
+++ lib/gui/conversion_gui.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python
 #    By Hernan Chavez Thielemann
 __author__ = 'Hernan Chavez Thielemann <hchavezthiele at gmail dot com>'
-# checked ok 04/05/2018 
+# checked ok 27/01/2019
 
 #------------------------------------------------------
 #///    Packages and globals definitions are here   ///
 #------------------------------------------------------
 
-from Tkinter import Frame, Label, IntVar, Radiobutton, StringVar
-from Tkinter import Widget, Button
+from Tkinter import Frame, Label, StringVar, Button # , IntVar
 
-from popup import PromptPopUp, PromptPopUp_wck
 from tk_lib import bottom_hline_deco, format_dec, Drop_Down_List
 from tk_lib import createmenubar, create_entry, get_entriesvalue
 from custom_row import File_Entry
 
-from lib.misc.data import check_vars
 from lib.misc.warn import print_dec_g, pop_wrg_1, pop_err_1
 from lib.misc.file import check_file, check_in_file
 
@@ -60,14 +57,13 @@ class Conversion(Frame):
         
         #eg_files=['conf.gro','topol.top','forcefield.itp','nb.itp','bon.itp']
         _atomstyle_ = 'full'
-        _radio_ = 1
         
         data_cont = self.master._convert_['setup']
         if data_cont <> []:
             _autoload_ = data_cont[0]
-            eg_files = data_cont[1:-2]
-            _atomstyle_ = data_cont[-2]
-            _radio_ = data_cont[-1]
+            eg_files = data_cont[1:-1]
+            _atomstyle_ = data_cont[-1]
+
             
         if _autoload_:
             enabled = [ 1, 1, 0, 0, 0]
@@ -89,9 +85,9 @@ class Conversion(Frame):
         row.pack( side='top', fill='x', padx=5)
         bottom_hline_deco(self)
         
-        ## CS   files
+        ## CS   files                              ------------------------- :@
         # appends file entries capturing button also 
-        self.objt_c.append( 1) # allocating space for autoload in space [0]
+        self.objt_c.append( _autoload_) # allocating space for autoload in [0]
         for fi in range( len( eg_files)):
             self.file_e.append( File_Entry( self,
                                            e_txt = fi_txt[fi],
@@ -103,27 +99,22 @@ class Conversion(Frame):
             
             if fi == 1:
                 bottom_hline_deco( self)
-                ## CS   autoload
+                ## CS   autoload                   ------------------------- :@
                 # self.objt_c.append inside 
-                self.autoload_buffer = _autoload_
-                self.objt_c[0] = self.autoloadff()
-                self.objt_c[0].set( _autoload_)
+                self.autoload_buffer = ''
+                self.autoloadff()
+                
             
             
         bottom_hline_deco( self)
         
-        ## CS   atom style
+        ## CS   atom style                         ------------------------- :@
         # self.objt_c.append inside 
         bottom_hline_deco( self, self.atomstyle)
         self.objt_c[-1].set( _atomstyle_)
         
-        ## CS   solvation selector
-        # self.objt_c.append inside 
-        self.sol_buffer = _radio_
-        bottom_hline_deco( self, self.build_solvation_section)
-        self.objt_c[-1].set( _radio_)
         
-        #    Final Buttons
+        #    Final Buttons                         ------------------------- :@
         self.build_finalbuttons()
         
         ######     END CONSTRUCTION SITE  -> PACK OUTSIDE #########
@@ -131,54 +122,6 @@ class Conversion(Frame):
         if self.master.test:
             print 'Seeing main gro2lam converter GUI'
             self.after(2000, self.master.swap_hook  )
-
-    def build_solvation_section( self):
-        
-        row = Frame(self)
-        
-        self.radio_part( row, 'Solvation atoms', [' yes',' no'])
-        
-        self.solv_b = Button( row, image= self.img['gear'],
-                             width = 25, height=23,
-                             command= self.config_solvation
-                            )
-        self.solv_b.pack( side='right', padx=0)
-        row.pack( side='top', fill='x', pady=3)
-
-    def radio_part(self, _s_row_, _text_, _desc_=[''], _vals_=[]):
-        
-        _f_labels = format_dec([_s_row_, _text_])#, _pack_=False)
-        
-        radio_var = IntVar()
-        for d in range( len( _desc_)):
-            
-            label_rb = Radiobutton(_s_row_, width=0, text= _desc_[d]
-                                   ,variable = radio_var
-                                   , value= len( _desc_)-1-d
-                                   ,command = self.solvastuff
-                                   , anchor='w'
-                                  )
-            label_rb.pack(side='left', padx=6)
-        self.objt_c.append( radio_var)
-    
-    def solvastuff(self):
-        '''If this point is reached, some button changed '''
-        _autoload_ = self.objt_c[0].get()
-        _solvation_ = self.objt_c[-1].get()
-        
-        if not _autoload_ and self.sol_buffer <> _solvation_:
-            self.sol_buffer = _solvation_
-            
-            if _solvation_:
-                self.solv_b.configure( state = 'normal')
-            else:
-                pop_wrg_1('You are choosing to not consider solvation.'
-                                ,' If some solvent molecules are in the'
-                                +' .gro file they will be ignored!', _i_=0)
-                self.solv_b.configure( state = 'disabled')
-        elif _autoload_ and self.sol_buffer <> _solvation_:
-            self.sol_buffer = 1
-            self.objt_c[-1].set( 1)
 
     def atomstyle( self):
         ''' in this case just one, but could be modified 
@@ -216,104 +159,84 @@ class Conversion(Frame):
         stated in the top file, also as a logic side effect disables the 
         file load option for forcefields
         '''
-        _s_row_ = Frame(self)
+        _a_row_ = Frame(self)
         
         _text_ =  'Autoload forcefield files'
         
         # in a future release this can be done with "self.radio_part"
         # but there is the need to implenet it with function dependance
-        _desc_ = [' yes',' no']
-        _f_labels = format_dec([_s_row_, _text_])
         
-        radio_var = IntVar()
-        for d in range( len( _desc_)):
-            label_rb = Radiobutton(_s_row_, width=0, text= _desc_[d]
-                                   ,variable = radio_var
-                                   , value= len( _desc_)-1-d
-                                   ,command = self.autoloadstuff
-                                   , anchor='w'
-                                  )
-            label_rb.pack(side='left', padx=6)
+        bt_txt_ = 'Press to autoload'
+        _f_labels = format_dec([_a_row_, _text_])
         
-        _s_row_.pack( side='top', fill='x', pady=3)
-        return radio_var
+        self.autol_b = Button( _a_row_, text = bt_txt_,
+                             #width = 25, height=23,
+                             command= self.autoloadstuff
+                            )
+        self.autol_b.pack(  padx=0)#side='center',
+        _a_row_.pack( side='top', fill='x', pady=3)
 
     def autoloadstuff(self):
         
-        _autoload_ = self.objt_c[0].get()
-        if self.autoload_buffer <> _autoload_:
-            self.autoload_buffer = _autoload_
+        nonerr_flag = True
+        
+        #_autoload_ = self.objt_c[0].get()
+        main_top_file = self.objt_c[2].get()
+
+        if self.autoload_buffer <> main_top_file:
+        #    self.autoload_buffer = _autoload_
+            self.autoload_buffer = main_top_file
             
-            nonerr_flag = True
-            
-            if _autoload_:
-                pop_wrg_1('You are choosing to autoload the forcefield files.',
-                          ' ', _i_=0)
-                
-                ############ solvastuff
-                self.solv_b.configure( state = 'disabled')
-                self.sol_buffer = 1
-                self.objt_c[-1].set( 1)
-                
-                aux_cont = get_ffldfiles( self.objt_c[2].get())
-                
-                if aux_cont <> ['','','']:
-                    # asign and disable file_row 3 4 5
-                    for i in [2, 3, 4]:
-                        nonerr_flag *= check_file( aux_cont[i-2])
-                        self.file_e[i]._entry.delete(0, 'end')
-                        self.file_e[i]._entry.insert(0, aux_cont[i-2])
-                        self.file_e[i]._entry.xview_moveto(1)
-                        
-                        self.file_e[i].setter( 0)
-                        
-                else:
-                    nonerr_flag = False
-                        
-                        
-                if not nonerr_flag:
-                    pop_err_1('Autoload failed!')
-                    self.objt_c[0].set( 0)
-                    self.autoload_buffer = 0
-            else:
-                nonerr_flag = False
-            
-            if not nonerr_flag:
-                self.solv_b.configure( state = 'normal')
+            aux_cont, nonerr_flag = get_ffldfiles( main_top_file )
+            if nonerr_flag:
                 for i in [2, 3, 4]:
+                    nonerr_flag *= check_file( aux_cont[i-2])
+                    self.file_e[i]._entry.delete(0, 'end')
+                    self.file_e[i]._entry.insert(0, aux_cont[i-2])
+                    self.file_e[i]._entry.xview_moveto(1)
+                    
+                    self.file_e[i].setter( 0)
+                    
+                self.objt_c[0] = ( 1)
+            else:
+                self.objt_c[0] = ( 0)
+        elif self.objt_c[0] and self.autoload_buffer == main_top_file:
+            pop_wrg_1('Autoload already performed for selected top file.', _i_=0)
+                        
+                    
+                    
+        if not self.objt_c[0]:
+            pop_err_1('Autoload failed!')
+            self.autoload_buffer = ''
+            self.autol_b.configure( state = 'disabled')
+            for i in [2, 3, 4]:
                     self.file_e[i].setter( 1)
-            
+
     def build_finalbuttons(self):
         
-        Frame(self).pack(side="top", fill='x', pady=10) # Space
+        _ypadhere_ = 1
+        Frame(self).pack(side="top", fill='x', pady=35) # Space
         
         _row_= self
         self.b1 = Button(_row_, text='Convert',
                          command = self.getdata_and_convert)
-        self.b1.pack( side = 'right', padx=30, pady=10)
+        self.b1.pack( side = 'right', padx=30, pady= _ypadhere_)
         
         b2 = Button(_row_, text = 'Quit', command = self.quit)
-        b2.pack( side = 'right', padx=10, pady=10)
+        b2.pack( side = 'right', padx=10, pady= _ypadhere_)
 
     def getdata_and_convert(self):
-        _solvatedinfo_ = self.master._convert_['solvation']
-        _autoload_ = self.objt_c[0].get()
-        _solvation_ = self.objt_c[-1].get()
-        if ( not _autoload_ and _solvation_  and _solvatedinfo_ == []):
-            pop_wrg_1( 'First perform the solvation configuration.'
-                      + ' You can do it pressing the solvation settings gears',
-                      _i_=0
-                     )
-            self.solv_b.invoke()
-            
-        elif self.get_entriesvalues():
+        
+        
+        _autoload_ = self.objt_c[0]
+        
+        if self.get_entriesvalues():
             
             data_cont = self.master._convert_['setup']
-            config = data_cont[-2:]+[ _autoload_]
+            config = [data_cont[-1], 1, _autoload_]
             
-            sim_data, flag_done_ = extract_gromacs_data(data_cont[1:-2],
-                                                        _solvatedinfo_,
-                                                        config[1:])
+            sim_data, flag_done_ = extract_gromacs_data( data_cont[1:-1],
+                                                         _autoload_)
             if flag_done_:
                 try:
                     flag_done_ = write_lammps_data( sim_data, 'data.gro2lam',
@@ -360,9 +283,12 @@ class Conversion(Frame):
         ent_rang  =  range(len(self.objt_c))
         
         for ent in ent_rang:#[1:]:
-            e_values.append(self.objt_c[ent].get())
-            if 0< ent and ent <= 5:
-                _flag_ *= check_file(e_values[-1])
+            if ent == 0:
+                e_values.append( self.objt_c[ent])
+            else:
+                e_values.append(self.objt_c[ent].get())
+                if ent and ent <= 5:
+                    _flag_ *= check_file(e_values[-1])
             
         self.master._convert_['setup'] = e_values
         return _flag_
@@ -371,99 +297,3 @@ class Conversion(Frame):
         ''' unified name hook'''
         self.create_conversion_gui()
 
-    def config_solvation( self):
-            
-        title_txt = ' '*15+'Input Water name'
-        instructions = 'Enter the atom tag letters of:'
-        askfor = ['O in the non bonded file',
-                  'H in the non bonded file',
-                  'O in the .gro file',
-                  'H in the .gro file',
-                  'H - O partial charge'
-                  #'Na+ in the .gro file (if are present)',
-                  #'Cl- in the .gro file (if are present)'
-                 ]
-        _solvatedinfo_ = self.master._convert_['solvation']
-        
-        if _solvatedinfo_ ==  []:
-            defaultanswer = ['opls_116','opls_117','OW','HW1, HW2','0.4238'
-                            #,'Na','Cl'
-                           ]
-        else:
-            defaultanswer = _solvatedinfo_
-            
-        self.solv_b.config(bg = 'gray40', width = 45) #cyan')
-        self.master._aux_ = []
-        
-        pop = PromptPopUp(master = self.master,
-                          title = title_txt,
-                          briefing = instructions, 
-                          entries_txt = askfor, 
-                          entries_val = defaultanswer
-                         )
-        
-        pop.wait_window()
-        
-        if self.master._aux_ <> [] and self.get_entriesvalues():
-            _app_aux_ = self.master._aux_
-            
-            _flag_ = self.check_solvation_inputs( _app_aux_, True)
-            if _flag_:
-                self.master._convert_['solvation'] = _app_aux_
-                print_dec_g('Solvation setup saved!')
-                #self.solv_b.focus()
-        self.solv_b.config(bg = 'lightgrey', width = 28)
-
-    def check_solvation_inputs( self, _app_aux_, _vbs_ = False):
-        ''' ================    Check inputs    ================= '''
-        _flag_ = 1
-        nb_Ox, nbHy, gro_Oxs, gro_Hys, pch= _app_aux_
-        _,_gro_f, _, _, _nbn_f, _, _, _= self.master._convert_['setup']
-        _flags = check_in_file( _nbn_f, nb_Ox, nbHy, pstn = 0)
-        print ([nb_Ox, nbHy])
-        
-        if min(_flags)==0:
-            print nb_Ox, nbHy
-            
-        list_aux = [Ox.strip(' ') for Ox in gro_Oxs.split(',')]
-        list_aux += [Ox.strip(' ') for Ox in gro_Hys.split(',')]
-        _flags += check_in_file( _gro_f, *list_aux, slce = '10:15')
-        print (list_aux)
-        
-        if min(_flags[2:])==0:
-            print list_aux
-            
-        try:
-            x = float(pch)
-            if -10<x<10:
-                _flags += [1]
-                partial_charges = 'Partial charges for H: {} and for O: {}'
-                print partial_charges.format( x, x*-2)
-            else:
-                _flags += [0]
-        except:
-            _flags += [0]
-        list_aux = [nb_Ox, nbHy] + list_aux
-        
-        # ================= Now check the flags =====================
-        for v in range(len(_flags)):
-            err_txt = ''
-            if not _flags[v] and v <> (len(_flags)-1):
-                filename = _nbn_f
-                if v>1:
-                    filename = _gro_f
-                err_txt = 'Atom tag {} not found in {}'
-                if '/' in filename:
-                    filename = filename.split('/')[-1]
-                err_txt = err_txt.format( list_aux[v], filename)
-                _flag_*=0
-            
-            elif not _flags[v]:
-                err_txt = 'Non valid partial charge {}'.format( pch)
-                _flag_*=0
-                
-            if err_txt<>'':
-                pop_err_1(err_txt+'\nSetup not saved')
-        #============       Done!  ==================================
-        return _flag_
-    
