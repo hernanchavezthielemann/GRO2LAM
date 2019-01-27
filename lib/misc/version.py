7740bf33aabe0b09d9a3fc11e92549caef6b772c
Hernan Chavez Thielemann <herchavezt@gmail.com>
2019-01-27 18:55:10 +0100

    Update version.py

--- lib/misc/version.py
+++ lib/misc/version.py
@@ -2,9 +2,9 @@
 
 # pitiful file, but neats
 
-__curr_ver__ = '1.12 20 Dic 2018'.split()
+__curr_ver__ = '1.2 27 Jan 2019'.split()
 
-__old_vers__ = ['1.11 20 Jul 2018', '1.1 07 May 2018','1.0 13 Apr 2018']
+__old_vers__ = ['1.12 20 Dic 2018', '1.11 20 Jul 2018', '1.1 07 May 2018','1.0 13 Apr 2018']
 
 __url__ = 'https://github.com/hernanchavezthielemann/GRO2LAM'
 
